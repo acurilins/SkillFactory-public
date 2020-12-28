358ace53347c6dd4f31739900e5de45c17e21030
acurilins <68602525+acurilins@users.noreply.github.com>
2020-12-28 21:47:26 +0400

    Modul_19_homework

---
+++ project_19/19.7.2/settings.py
@@ -0,0 +1,7 @@
+valid_email = "abra@cadabra.com"
+valid_password = "abra@cadabra.com"
+
+invalid_email = "thereisnosuchemail.com"
+invalid_password = "andthepasswordaswell"
+
+invalid_auth_key = "thereisnosuchauthenticationkey"
