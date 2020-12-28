c892538f2a3ed33691dd93cc7f18474763dbf6d3
acurilins <68602525+acurilins@users.noreply.github.com>
2020-12-28 21:50:17 +0400

    files_removed

--- project_19/19.7.2/settings.py
+++
@@ -1,7 +0,0 @@
-valid_email = "abra@cadabra.com"
-valid_password = "abra@cadabra.com"
-
-invalid_email = "thereisnosuchemail.com"
-invalid_password = "andthepasswordaswell"
-
-invalid_auth_key = "thereisnosuchauthenticationkey"
